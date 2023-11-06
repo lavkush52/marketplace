e957ba28015d18befc20bdb0784dd14a97c8a0ea
asad <asad@agreedtechnologies.com>
2023-11-06 20:35:58 +0530

    asas

--- application/controllers/admin/Layout.php
+++ application/controllers/admin/Layout.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Layout extends CI_Controller{
     
 	public function switcher(){
-          $this->load->view('switcher');
+          $this->load->view('layout/common/switcher');
 
 	}
 
